0320fdedce2f560e1c6e08a0f30ee75ff4e05d98
zo0bb <serreslouis31@gmail.com>
2026-03-13 08:18:47 +0100

    Ajout de test fonctionnel

---
+++ src/test_fonctionnel/TestGaulois.java
@@ -0,0 +1,14 @@
+package test_fonctionnel;
+
+import personnages.Gaulois;
+
+public class TestGaulois {
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("asterix", 8);
+		Gaulois obelix = new Gaulois("obelix", 16);
+		asterix.parler("Bonjour Obélix.");
+		obelix.parler("Bonjour Astérix. Ca te dirais d'aller chasser des sangliers ?");
+		asterix.parler("Oui très bonne idée");
+	}
+	
+}
