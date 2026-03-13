ff5fdea9395692cc8f3e50e57ebc8af226e7951a
zo0bb <serreslouis31@gmail.com>
2026-03-13 09:42:23 +0100

    Fin de TP partie 4 à terminer chez soi

--- src/objets/Chaudron.java
+++ src/objets/Chaudron.java
@@ -1,5 +1,27 @@
 package objets;
 
 public class Chaudron {
-
+	public Chaudron(int quantitePotion, int forcePotion) {
+		super();
+		this.quantitePotion = quantitePotion;
+		this.forcePotion = forcePotion;
+	}
+	private int quantitePotion;
+	private int forcePotion;
+	
+	public void remplirChaudron(int quantite, int forcePotion) {
+		quantitePotion = quantite;
+		forcePotion = forcePotion;
+	}
+	
+	public boolean resterPotion() {
+		if (quantitePotion==0) {
+			return false;
+		}
+		return true;
+	}
+	
+	public void prendreLouche() {
+		
+	}
 }
--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,24 +1,38 @@
 package personnages;
 
+import objets.Chaudron;
+
 public class Druide {
+
+	private String nom;
+	private int force;
+	private Chaudron chaudron = new Chaudron(5, 5);
+
 	public Druide(String nom, int force) {
 		super();
 		this.nom = nom;
 		this.force = force;
 	}
 
-	private String nom;
-	private int force;
-	
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public void parler(String texte) {
-		System.out.println(prendreParole = + "\"" + texte + "\"");
+		System.out.println(prendreParole() + "\"" + texte + "\"");
 	}
-	
+
 	private String prendreParole() {
-		return "Le romain " + nom + " : ";
+		return "Le druide " + nom + " : ";
+	}
+
+	public void fabriquerPotion(int quantite, int forcePotion) {
+		parler("J'ai concocté " + quantite + " doses de potion magique. Elle a une force de " + forcePotion + ".");
+	}
+
+	public void booster(Gaulois gaulois) {
+		if (chaudron.resterPotion()) {
+
+		}
 	}
 }
