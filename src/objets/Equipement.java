12a42e79846d4ca0508d1f12310d99be55ddc9fc
Louis SERRES <serreslouis31@gmail.com>
2026-04-21 11:14:43 +0200

    Fin de partie 3 (enum)

---
+++ src/objets/Equipement.java
@@ -0,0 +1,17 @@
+package objets;
+
+public enum Equipement {
+	CASQUE("casque"),BOUCLIER("bouclier");
+	
+	private String nom;
+	
+	
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+	
+	public String toString() {
+		return nom;
+	}
+	
+}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,21 +1,26 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
+	
+	private String nom;
+	private int force;
+	private Equipement[] tabEquipements;
+	private int nbEquipement = 0;
+	
+	
 	public Romain(String nom, int force) {
 		super();
 		this.nom = nom;
 		this.force = force;
+		this.tabEquipements = new Equipement[2];
 		assert isInvariantVerified();
 	}
 	
 	private boolean isInvariantVerified() {
 		return force>=0;
 	}
-	
-
-	private String nom;
-	private int force;
-	
 
 	public String getNom() {
 		return nom;
@@ -49,9 +54,40 @@ public class Romain {
 		assert force < forceBase;
 	}
 	
+	public int sEquiper(Equipement equipement) {
+		String presentation = "Le soldat " + nom;
+		switch (nbEquipement) {
+		
+		case 2:
+			System.out.println( presentation + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (tabEquipements[0]==equipement) {
+				System.out.println(presentation + " possède déjà un " + equipement + " ! ");
+			}
+			else {
+				tabEquipements[1]=equipement;
+				nbEquipement++;
+				System.out.println(presentation + " s'équipe avec un " + equipement + ". ");
+			}
+			break;
+			
+		default:
+			tabEquipements[0]=equipement;
+			nbEquipement++;
+			System.out.println( presentation + " s'équipe avec un " + equipement + ". ");
+			break;
+
+		}
+		return nbEquipement;
+	}
+	
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
-		
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 	
 	
